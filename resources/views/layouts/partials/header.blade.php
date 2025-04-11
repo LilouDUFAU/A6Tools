156dd426323675eae85dff3a251e717523706502
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-04-11 15:50:49 +0200

    ✨ Ajout d'un espacement entre les éléments du menu et amélioration des styles de la section de connexion

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,4 +1,4 @@
-<div class="flex items-center justify-between p-4">
+<div class="flex items-center justify-between p-4 gap-4">
     <form action="/search" method="GET" class="w-full max-w-md">
         <div class="relative">
             <input type="text" name="query" placeholder="Rechercher..." class="w-full px-4 py-2 border border-gray-300 rounded-lg outline-none"/>
@@ -8,10 +8,10 @@
         </div>
     </form>
 
-    <a href="/login" class="text-gray-700 hover:text-green-600">
-        <div class="hidden md:flex items-center gap-4 p-4">
+    <a href="/login" class="hidden md:flex bg-green-600 text-white rounded-lg hover:bg-green-700 transition-all duration-300 items-center gap-2 px-2 py-1">
+        <div class="flex items-center gap-4 px-2 py-1 transition-all duration-300">
             <p class="">Connexion</p>
-            <i class="fa-solid fa-circle-user text-2xl"></i>
+            <i class="fa-solid fa-circle-user text-2xl text-white"></i>
         </div>
     </a>
 </div>
