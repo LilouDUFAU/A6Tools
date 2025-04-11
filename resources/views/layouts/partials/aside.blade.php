9d9979a017fe94514c84e1c67e5037cca353e438
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-04-11 15:12:50 +0200

    ✨ Ajout de transitions CSS aux éléments du menu de navigation pour améliorer l'interaction utilisateur

--- resources/views/layouts/partials/aside.blade.php
+++ resources/views/layouts/partials/aside.blade.php
@@ -1,12 +1,12 @@
 <nav>
     <ul class="flex flex-col p-4 gap-4">
         <li class="hidden md:block"><a><img src="{{ asset('logo_a6tools.png') }}" alt="Logo" class="w-16 h-auto mb-2 md:mb-4"></a></li>
-        <li class="hover:bg-gray-600 hover:bg-opacity-75 rounded-lg hover:px-2 hover:py-1 hover:text-white"><a href="/"><i class="fa-solid fa-house"></i> <span class="ml-2">Home</span></a></li>
-        <li class="hover:bg-gray-600 hover:bg-opacity-75 rounded-lg hover:px-2 hover:py-1 hover:text-white"><a href="/about"><i class="fa-solid fa-table"></i> <span class="ml-2">Dashboard</span></a></li>
-        <li class="hover:bg-gray-600 hover:bg-opacity-75 rounded-lg hover:px-2 hover:py-1 hover:text-white"><a href="/services"><i class="fa-solid fa-box-open"></i> <span class="ml-2">Gestock</span></a></li>
-        <li class="hover:bg-gray-600 hover:bg-opacity-75 rounded-lg hover:px-2 hover:py-1 hover:text-white"><a href="/contact"><i class="fa-solid fa-desktop"></i> <span class="ml-2">GestRenouv</span></a></li>
-        <li class="hover:bg-gray-600 hover:bg-opacity-75 rounded-lg hover:px-2 hover:py-1 hover:text-white"><a href="/services"><i class="fa-solid fa-wrench"></i> <span class="ml-2">GestAtelier</span></a></li>
-        <li class="hover:bg-gray-600 hover:bg-opacity-75 rounded-lg hover:px-2 hover:py-1 hover:text-white"><a href="/contact"><i class="fa-solid fa-bolt"></i> <span class="ml-2">GestSAV</span></a></li>
-        <li class="block md:hidden hover:bg-gray-600 hover:bg-opacity-75 rounded-lg hover:px-2 hover:py-1 hover:text-white"><a href="/login"><i class="fa-light fa-circle-user"></i> <span class="ml-2">Connexion</span></a></li>
+        <li class="hover:bg-gray-600 hover:bg-opacity-75 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><a href="/"><i class="fa-solid fa-house"></i> <span class="ml-2">Home</span></a></li>
+        <li class="hover:bg-gray-600 hover:bg-opacity-75 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><a href="/about"><i class="fa-solid fa-table"></i> <span class="ml-2">Dashboard</span></a></li>
+        <li class="hover:bg-gray-600 hover:bg-opacity-75 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><a href="/services"><i class="fa-solid fa-box-open"></i> <span class="ml-2">Gestock</span></a></li>
+        <li class="hover:bg-gray-600 hover:bg-opacity-75 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><a href="/contact"><i class="fa-solid fa-desktop"></i> <span class="ml-2">GestRenouv</span></a></li>
+        <li class="hover:bg-gray-600 hover:bg-opacity-75 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><a href="/services"><i class="fa-solid fa-wrench"></i> <span class="ml-2">GestAtelier</span></a></li>
+        <li class="hover:bg-gray-600 hover:bg-opacity-75 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><a href="/contact"><i class="fa-solid fa-bolt"></i> <span class="ml-2">GestSAV</span></a></li>
+        <li class="block md:hidden hover:bg-gray-600 hover:bg-opacity-75 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><a href="/login"><i class="fa-light fa-circle-user"></i> <span class="ml-2">Connexion</span></a></li>
     </ul>
 </nav>
