edf76cd81ac1d6b2391c533c7e7624dead635487
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-04-11 14:59:36 +0200

    ✨ Ajout du menu de navigation basic dans le fichier aside

---
+++ resources/views/layouts/partials/aside.blade.php
@@ -0,0 +1,12 @@
+<nav>
+    <ul class="flex flex-col p-4 gap-4">
+        <li class="hidden md:block"><a><img src="{{ asset('logo_a6tools.png') }}" alt="Logo" class="w-16 h-auto mb-2 md:mb-4"></a></li>
+        <li><a href="/"><i class="fa-solid fa-house"></i> <span class="ml-2">Home</span></a></li>
+        <li><a href="/about"><i class="fa-solid fa-table"></i> <span class="ml-2">Dashboard</span></a></li>
+        <li><a href="/services"><i class="fa-solid fa-box-open"></i> <span class="ml-2">Gestock</span></a></li>
+        <li><a href="/contact"><i class="fa-solid fa-desktop"></i> <span class="ml-2">GestRenouv</span></a></li>
+        <li><a href="/services"><i class="fa-solid fa-wrench"></i> <span class="ml-2">GestAtelier</span></a></li>
+        <li><a href="/contact"><i class="fa-solid fa-bolt"></i> <span class="ml-2">GestSAV</span></a></li>
+        <li class="block md:hidden"><a href="/login"><i class="fa-solid fa-circle-user"></i> <span class="ml-2">Connexion</span></a></li>
+    </ul>
+</nav>
