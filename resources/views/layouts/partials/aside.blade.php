241ce889e78030e0496120027c040d4f39ba42d4
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-04-11 16:34:20 +0200

    ✨ Correction de la classe d'affichage pour le lien "Home" dans la barre latérale

--- resources/views/layouts/partials/aside.blade.php
+++ resources/views/layouts/partials/aside.blade.php
@@ -1,7 +1,7 @@
 <nav>
     <ul class="flex flex-col p-4 gap-4">
         <li class="hidden md:flex justify-center"><a><img src="{{ asset('/images/logo_a6tools.png') }}" alt="Logo" class="w-64 h-auto mb-2 md:mb-4"></a></li>
-        <li class="hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><a href="/"><i class="fa-solid fa-house"></i> <span class="ml-2">Home</span></a></li>
+        <li class="flex md:hidden hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><a href="/"><i class="fa-solid fa-house"></i> <span class="ml-2">Home</span></a></li>
         <li class="hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><a href="/about"><i class="fa-solid fa-table"></i> <span class="ml-2">Dashboard</span></a></li>
         <li class="hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><a href="/services"><i class="fa-solid fa-box-open"></i> <span class="ml-2">Gestock</span></a></li>
         <li class="hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><a href="/contact"><i class="fa-solid fa-desktop"></i> <span class="ml-2">GestRenouv</span></a></li>
