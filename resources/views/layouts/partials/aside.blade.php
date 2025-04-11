b44f3251a6c2fa07aaa55b73d5958ac335288fa4
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-04-11 16:54:23 +0200

    ✨ Réorganisation des liens dans la barre latérale pour une meilleure structure

--- resources/views/layouts/partials/aside.blade.php
+++ resources/views/layouts/partials/aside.blade.php
@@ -1,12 +1,12 @@
 <nav>
     <ul class="flex flex-col p-4 gap-4">
-        <li class="hidden md:flex justify-center"><a><img src="{{ asset('/images/logo_a6tools.png') }}" alt="Logo" class="w-64 h-auto mb-2 md:mb-4"></a></li>
-        <li class="flex md:hidden hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><a href="/"><i class="fa-solid fa-house"></i> <span class="ml-2">Home</span></a></li>
-        <li class="hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><a href="/about"><i class="fa-solid fa-table"></i> <span class="ml-2">Dashboard</span></a></li>
-        <li class="hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><a href="/services"><i class="fa-solid fa-box-open"></i> <span class="ml-2">Gestock</span></a></li>
-        <li class="hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><a href="/contact"><i class="fa-solid fa-desktop"></i> <span class="ml-2">GestRenouv</span></a></li>
-        <li class="hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><a href="/services"><i class="fa-solid fa-wrench"></i> <span class="ml-2">GestAtelier</span></a></li>
-        <li class="hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><a href="/contact"><i class="fa-solid fa-bolt"></i> <span class="ml-2">GestSAV</span></a></li>
-        <li class="block md:hidden hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><a href="/login"><i class="fa-solid fa-circle-user"></i> <span class="ml-2">Connexion</span></a></li>
+        <a href="/about" class="hidden md:flex justify-center"><img src="{{ asset('/images/logo_a6tools.png') }}" alt="Logo" class="w-64 h-auto mb-2 md:mb-4"></a>
+        <a href="/about" class="flex md:hidden hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><i class="fa-solid fa-house"></i> <span class="ml-2">Home</span></a>
+        <a href="/about" class="hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><i class="fa-solid fa-table"></i> <span class="ml-2">Dashboard</span></a>
+        <a href="/about" class="hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><i class="fa-solid fa-box-open"></i> <span class="ml-2">Gestock</span></a>
+        <a href="/about" class="hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><i class="fa-solid fa-desktop"></i> <span class="ml-2">GestRenouv</span></a>
+        <a href="/about" class="hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><i class="fa-solid fa-wrench"></i> <span class="ml-2">GestAtelier</span></a>
+        <a href="/about" class="hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><i class="fa-solid fa-bolt"></i> <span class="ml-2">GestSAV</span></a>
+        <a href="/about" class="block md:hidden hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300"><i class="fa-solid fa-circle-user"></i> <span class="ml-2">Connexion</span></a>
     </ul>
 </nav>
