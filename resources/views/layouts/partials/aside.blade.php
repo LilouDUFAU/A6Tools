6038c7d804af79fcbc1ead702e55091933b4461b
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-04-14 11:44:26 +0200

    ✨ Update 'Accueil' link in aside view to use the welcome route for improved navigation

--- resources/views/layouts/partials/aside.blade.php
+++ resources/views/layouts/partials/aside.blade.php
@@ -1,7 +1,7 @@
 <nav>
     <ul class="flex flex-col p-4 gap-4">
         <a href="{{ route('welcome') }}" class="hidden md:flex justify-center"><img src="{{ asset('/images/logo_a6tools.png') }}" alt="Logo" class="w-64 h-auto mb-2 md:mb-4"></a>
-        <a href="" class="flex md:hidden hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300 hover:font-semibold"><i class="fa-solid fa-house"></i> <span class="ml-2">Accueil</span></a>
+        <a href="{{ route('welcome') }}" class="flex md:hidden hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300 hover:font-semibold"><i class="fa-solid fa-house"></i> <span class="ml-2">Accueil</span></a>
         <a href="" class="hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300 hover:font-semibold"><i class="fa-solid fa-table"></i> <span class="ml-2">Dashboard</span></a>
         <a href="" class="hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300 hover:font-semibold"><i class="fa-solid fa-box-open"></i> <span class="ml-2">Gestock</span></a>
         <a href="" class="hover:bg-green-700 rounded-lg hover:px-2 hover:py-1 hover:text-white transition-all duration-300 hover:font-semibold"><i class="fa-solid fa-desktop"></i> <span class="ml-2">GestRenouv</span></a>
