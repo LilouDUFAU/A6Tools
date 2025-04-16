814a65d5d3e50fc648cb32a3e8ebdcde8ab651eb
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-04-16 16:32:24 +0200

    ✨ Enhance produit relationship: include pivot data and timestamps for better product management in commandes

--- app/Models/Commande.php
+++ app/Models/Commande.php
@@ -75,6 +75,6 @@ class Commande extends Model
     //une commande peut contenir plusieurs produits
     public function produits()
     {
-        return $this->belongsToMany(Produit::class, 'commande_produit');
+        return $this->belongsToMany(Produit::class, 'commande_produit')->withPivot('quantite', 'quantite_stock', 'quantite_client')->withTimestamps();
     }
 }
