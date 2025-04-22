847778ca78c4ac3884cca246aae23b0c13f11a66
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-04-22 11:39:30 +0200

    ✨ Add RoleSeeder: implement seeder for roles table to facilitate database seeding

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('roles')->insert([
+            ['id' => 1, 'nom' => 'admin', 'description' => 'Role permettant d\'administrer le système', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 2, 'nom' => 'user', 'description' => 'Role permettant d\'utiliser les fonctionnalités de base du système', 'created_at' => now(), 'updated_at' => now()],
+        ]);
+    }
+}
