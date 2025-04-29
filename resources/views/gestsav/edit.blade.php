d5f50d9b1d95bd2ed553994050ada5522bc23413
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-04-29 11:27:05 +0200

    🐛 Remove unused action input fields from edit form for cleaner UI

--- resources/views/gestsav/edit.blade.php
+++ resources/views/gestsav/edit.blade.php
@@ -94,14 +94,6 @@
             </select>
         </div>
         @endforeach
-        <div class="flex space-x-2 mb-2">
-            <input type="text" name="new_actions[]" placeholder="Nouvelle action" class="block w-full border-gray-300 rounded-lg shadow-sm focus:ring-green-500 focus:border-green-500 px-2 py-1">
-            <select name="new_status[]" class="border-gray-300 rounded-lg shadow-sm focus:ring-green-500 focus:border-green-500 px-2 py-1">
-                @foreach ($statut as $statut_option)
-                    <option value="{{ $statut_option }}" {{ $loop->first ? 'selected' : '' }}>{{ ucfirst($statut_option) }}</option>
-                @endforeach
-            </select>
-        </div>
     </div>
     <div class="text-right mt-4">
         <button type="button" onclick="addActionField()" class="bg-green-600 text-white px-4 py-2 rounded-lg shadow hover:bg-green-700 focus:ring-2 focus:ring-green-500">Ajouter une action</button>
