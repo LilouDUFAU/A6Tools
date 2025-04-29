ec372244665a7d5985df2a3c729a95cb90934b5c
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-04-29 11:36:47 +0200

    🐛 Fix form structure by correcting the placement of the closing div tag for better layout and functionality

--- resources/views/gestsav/create.blade.php
+++ resources/views/gestsav/create.blade.php
@@ -146,11 +146,13 @@ function removeActionField(button) {
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"/><polyline points="17 21 17 13 7 13 7 21"/><polyline points="7 3 7 8 15 8"/></svg>
             Enregistrer
         </button>
-        </div>
-    </form>
-    <div class="text-right mt-4 p-4">
+        <div class="text-right mt-4 p-4">
         <a href="{{ route('panne.index') }}" class="text-gray-500 hover:underline">Retour</a>
     </div>
+        </div>
+        
+    </form>
+    
 </div>
 <script>
 let clients = @json($clients);
