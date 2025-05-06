4506195740875e5f7a0d436369e498e841cde9f3
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-05-06 10:20:36 +0200

    🐛 Add UserSeeder to DatabaseSeeder for initial user data seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             ServiceSeeder::class,
             RoleSeeder::class,
             StockSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
