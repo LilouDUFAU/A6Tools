e2190cf81d575f23863acd544e58595504419e9d
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-05-12 10:44:52 +0200

    fix: Update table header label from 'N° commande' to 'N° cmde fournisseur'

--- resources/views/gestcommande/index.blade.php
+++ resources/views/gestcommande/index.blade.php
@@ -88,7 +88,7 @@
             <table class="min-w-full bg-white shadow-md rounded-lg">
                 <thead>
                     <tr id="table-headers" class="bg-gray-100 text-left text-sm font-semibold text-gray-700">
-                        <th class="py-3 px-4 border border-gray-200">N° commande</th>
+                        <th class="py-3 px-4 border border-gray-200">N° cmde fournisseur</th>
                         <th class="py-3 px-4 border border-gray-200">Client</th>
                         <th class="py-3 px-4 border border-gray-200">Fournisseur</th>
                         <th class="py-3 px-4 border border-gray-200">Site</th>
@@ -222,7 +222,7 @@
     };
 
     const defaultHeaders = `
-        <th class="py-3 px-4 border border-gray-200">N° commande</th>
+        <th class="py-3 px-4 border border-gray-200">N° cmde fournisseur</th>
         <th class="py-3 px-4 border border-gray-200">Client</th>
         <th class="py-3 px-4 border border-gray-200">Fournisseur</th>
         <th class="py-3 px-4 border border-gray-200">Site</th>
