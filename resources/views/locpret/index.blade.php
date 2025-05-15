f8be69ca6be5e4bd6c42d18b6a7df0edee6f81b7
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-05-15 16:17:33 +0200

    feat: Add navigation link to return to PC RenouvO in index view

--- resources/views/locpret/index.blade.php
+++ resources/views/locpret/index.blade.php
@@ -70,6 +70,7 @@
         </table>
     </div>
 </div>
+<a href=" {{route('gestrenouv.index') }}" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">Retour vers les PC RenouvO</a>
 
 <!-- Modals -->
 @foreach($locPrets as $locPret)
@@ -92,6 +93,7 @@
     </div>
 </div>
 
+
 <!-- Delete Modal -->
 <div id="deleteModal{{ $locPret->id }}" class="hidden fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
     <div class="bg-white rounded-lg shadow-lg max-w-md w-full p-6">
@@ -108,8 +110,7 @@
                 <button type="submit" class="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700">Supprimer</button>
             </form>
         </div>
-    </div>
-</div>
+    </div></div>
 @endforeach
 
 <script>
