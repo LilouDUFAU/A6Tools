f97cd3e3a8a2568e5289cb7fdabd489d152e6d78
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-05-15 16:39:13 +0200

    feat: Highlight client code in index view for PC Renouv

--- resources/views/gestrenouv/index.blade.php
+++ resources/views/gestrenouv/index.blade.php
@@ -66,7 +66,7 @@
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
                                     @if($client)
-                                        {{ $client->code_client }} ({{ $client->nom }})
+                                        <strong>{{ $client->code_client }}</strong> ({{ $client->nom }})
                                     @else
                                         -
                                     @endif
