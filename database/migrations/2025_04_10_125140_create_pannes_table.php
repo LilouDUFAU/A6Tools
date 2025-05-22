2b51330617b703495d775c831e0f232a85c84e0b
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-05-22 15:47:35 +0200

    feat: Add 'En attente' status to Panne model and update demande field to be non-nullable

--- database/migrations/2025_04_10_125140_create_pannes_table.php
+++ database/migrations/2025_04_10_125140_create_pannes_table.php
@@ -14,14 +14,14 @@ return new class extends Migration
         Schema::create('pannes', function (Blueprint $table) {
             $table->id();
             $table->string('numero_sav')->unique()->nullable(); // Numéro de SAV
-            $table->enum('statut', ['Remboursement', 'Transit', 'Envoyé', 'Échange anticipé']); // Statut de la panne
-            $table->string('demande')->nullable(); // Demande du client
+            $table->enum('statut', ['En attente', 'Remboursement', 'Transit', 'Envoyé', 'Échange anticipé']); // Statut de la panne
+            $table->string('demande'); // Demande du client
             $table->enum('etat_client', ['Ordi de prêt', 'Échangé', 'En attente']); // Etat de la panne vu par le client
-            $table->string('categorie_materiel'); // Catégorie du matériel concerné par la panne
-            $table->string('categorie_panne'); // Catégorie de la panne
-            $table->text('detail_panne'); // Détails sur la panne
-            $table->date('date_commande'); // Date de la commande du matériel
-            $table->date('date_panne'); // Date de la panne
+            $table->string('categorie_materiel')->nullable(); // Catégorie du matériel concerné par la panne
+            $table->string('categorie_panne')->nullable(); // Catégorie de la panne
+            $table->text('detail_panne')->nullable(); // Détails sur la panne
+            $table->date('date_commande')->nullable(); // Date de la commande du matériel
+            $table->date('date_panne')->nullable(); // Date de la panne
             $table->unsignedBigInteger('fournisseur_id'); // Clé étrangère vers la table fournisseurs
             $table->timestamps(); // created_at, updated_at
 
